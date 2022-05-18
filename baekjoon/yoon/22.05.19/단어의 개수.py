07de9ee1da42f4f5c705bef026a50c3b8020b3d9
prowh08 <pro_wh08@naver.com>
2022-05-19 07:50:54 +0900

    [yjh]2022.05.19

---
+++ baekjoon/yoon/22.05.19/단어의 개수.py
@@ -0,0 +1,7 @@
+n = input().split()
+cnt=0
+
+for i in n:
+    if i!='':
+        cnt+=1
+print(cnt)
---
+++ programmers/yoon/22.05.19/내적.py
@@ -0,0 +1,5 @@
+def solution(a, b):
+    answer=0
+    for a,b in zip(a,b):
+        answer+=a*b
+    return answer
